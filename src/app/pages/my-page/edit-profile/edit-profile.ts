8491ecae10da20d786f09cc62dcb8d56d195f32e
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-08-15 19:11:49 -0700

    250815: Modify editPro UI

--- src/app/pages/my-page/edit-profile/edit-profile.ts
+++ src/app/pages/my-page/edit-profile/edit-profile.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './edit-profile.html',
   styleUrl: './edit-profile.css'
 })
-export class EditProfile {
 
+export class EditProfile {
+  updateForm = new FormGroup({
+    firstName: new FormControl("", [
+      Validators.required,
+      Validators.minLength(2)
+    ]),
+    lastName: new FormControl("", [
+      Validators.required,
+      Validators.minLength(2)
+    ]),
+    email: new FormControl("", [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+      //passwordStrengthValidator
+    ]),
+    birthdate: new FormControl("", [
+      Validators.required,
+      //ageRangeValidator
+    ])
+  })
+  get firstName() { return this.updateForm.get('firstName'); }
+get lastName() { return this.updateForm.get('lastName'); }
+get email() { return this.updateForm.get('email'); }
+get password() { return this.updateForm.get('password'); }
+get birthdate() { return this.updateForm.get('birthdate'); }
+
+  // function  passwordStrengthValidator {
+    
+  // }
+
+  // function  ageRangeValidator {
+    
+  // }
+
+
+  onSubmit() {
+    if (this.updateForm.valid) {
+      //
+    }
+  }
 }
