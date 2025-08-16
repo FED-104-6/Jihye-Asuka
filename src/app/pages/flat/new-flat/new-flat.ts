6c6b1fb930ffb65826238a5b01874260594ab572
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-08-16 10:55:28 -0700

    250816: creat newflat UI

--- src/app/pages/flat/new-flat/new-flat.ts
+++ src/app/pages/flat/new-flat/new-flat.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-flat',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-flat.html',
   styleUrl: './new-flat.css'
 })
-export class NewFlat {
 
+export class NewFlat {
+  newFlatForm = new FormGroup({
+    built: new FormControl("", [
+      Validators.required
+    ]),
+    city: new FormControl("", [
+      Validators.required
+    ]),
+    dateAvailable: new FormControl("", [
+      Validators.required
+    ]),
+    hasAC: new FormControl("", [
+      Validators.required
+    ]),
+    price: new FormControl("", [
+      Validators.required
+    ]),
+    size: new FormControl("", [
+      Validators.required
+    ]),
+    stName: new FormControl("", [
+      Validators.required
+    ]),
+    stNum: new FormControl("", [
+      Validators.required
+    ])
+  })
+
+  onSubmit() {
+    if (this.newFlatForm.valid) {
+      alert('Your flat has been successfully registered!');
+    } else{
+      alert('Please fill in all the required information')
+    }
+  }
 }
