b777543661d331406fc0f288abb611dd3285b2af
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-08-16 19:42:17 -0700

    250816: create editflatform in TS

--- src/app/pages/flat/edit-flat/edit-flat.ts
+++ src/app/pages/flat/edit-flat/edit-flat.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-flat',
@@ -7,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './edit-flat.css'
 })
 export class EditFlat {
+editFlatForm = new FormGroup({
+    built: new FormControl("", [
+      Validators.required
+    ]),
+    city: new FormControl("", [
+      Validators.required
+    ]),
+    dateAvailable: new FormControl("", [
+      Validators.required
+    ]),
+    hasAC: new FormControl("", [
+      Validators.required
+    ]),
+    price: new FormControl("", [
+      Validators.required
+    ]),
+    size: new FormControl("", [
+      Validators.required
+    ]),
+    stName: new FormControl("", [
+      Validators.required
+    ]),
+    stNum: new FormControl("", [
+      Validators.required
+    ])
+  })
 
+  onSubmit() {
+    if (this.editFlatForm.valid) {
+      alert('Your flat has been successfully updated!');
+    } else{
+      alert('Please fill in all the required information')
+    }
+  }
 }
