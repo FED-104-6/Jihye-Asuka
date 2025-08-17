5856eb62d4fb4a6e88abd7e46085ed5390cae348
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-08-17 16:40:28 -0700

    250817: newflat add validation

--- src/app/pages/flat/new-flat/new-flat.ts
+++ src/app/pages/flat/new-flat/new-flat.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-flat',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-flat.html',
   styleUrl: './new-flat.css'
 })
@@ -11,10 +13,12 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angula
 export class NewFlat {
   newFlatForm = new FormGroup({
     built: new FormControl("", [
-      Validators.required
+      Validators.required,
+      Validators.min(0)
     ]),
     city: new FormControl("", [
-      Validators.required
+      Validators.required,
+      Validators.minLength(2)
     ]),
     dateAvailable: new FormControl("", [
       Validators.required
@@ -23,23 +27,43 @@ export class NewFlat {
       Validators.required
     ]),
     price: new FormControl("", [
-      Validators.required
+      Validators.required,
+      Validators.min(0)
     ]),
     size: new FormControl("", [
-      Validators.required
+      Validators.required,
+      Validators.min(0)
     ]),
     stName: new FormControl("", [
-      Validators.required
+      Validators.required,
+      Validators.minLength(2)
     ]),
     stNum: new FormControl("", [
-      Validators.required
+      Validators.required,
+      Validators.min(0)
     ])
   })
 
+  get built() { return this.newFlatForm.get('built'); }
+  get city() { return this.newFlatForm.get('city'); }
+  get dateAvailable() { return this.newFlatForm.get('dateAvailable'); }
+  get hasAC() { return this.newFlatForm.get('hasAC'); }
+  get price() { return this.newFlatForm.get('price'); }
+  get size() { return this.newFlatForm.get('size'); }
+  get stNum() { return this.newFlatForm.get('stNum'); }
+  get stName() { return this.newFlatForm.get('stName'); }
+
+  constructor(private router: Router) { }
+  goToHomePage() {
+    this.router.navigate(['/home'])
+  }
+
   onSubmit() {
     if (this.newFlatForm.valid) {
+      //TODO: Save new flat data into database
       alert('Your flat has been successfully registered!');
-    } else{
+      this.goToHomePage()
+    } else {
       alert('Please fill in all the required information')
     }
   }
