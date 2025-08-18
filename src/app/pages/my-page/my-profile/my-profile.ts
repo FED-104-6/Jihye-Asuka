b60b12dad5c93f92d9d969c7dd39762f0e5cdc6f
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-08-17 20:31:28 -0700

    250817: myprofile add ngoninit

--- src/app/pages/my-page/my-profile/my-profile.ts
+++ src/app/pages/my-page/my-profile/my-profile.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-my-profile',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './my-profile.html',
   styleUrl: './my-profile.css'
 })
@@ -16,6 +17,17 @@ export class MyProfile {
   birthDate = "1997/11/17";
 
   constructor(private router: Router) {}
+  // private userService: UserService
+
+  ngOnInit(){
+    // this.userService.getCurrentUser().subscribe(user => {
+    //   this.firstName = user.firstName;
+    //   this.lastName = user.lastName;
+    //   this.email = user.email;
+    //   this.birthDate = user.birthDate;
+    // });
+  }
+
   goToEditPage(){
     this.router.navigate(['/edit-profile'])
   }
