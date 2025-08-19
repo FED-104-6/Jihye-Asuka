52a2325d6695d1992d232748957397ecc7e3cf10
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-08-18 19:38:38 -0700

    250818: chenge myflats HTMl to bindStyle

--- src/app/pages/my-page/my-flats/my-flats.ts
+++ src/app/pages/my-page/my-flats/my-flats.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-flats',
-  imports: [],
+  imports: [DatePipe, CommonModule],
   templateUrl: './my-flats.html',
   styleUrl: './my-flats.css'
 })
 export class MyFlats {
-  
+  //test Data
+  myFlats = [
+    {
+      address: {
+        stNum: 233,
+        stName: 'Robson St',
+        city: 'Vancouver BC'
+      },
+      size: 56,
+      price: 24000,
+      dateAvailable: '2025-08-16',
+      img: '/assets/home.png'
+    },
+    {
+      address: {
+        stNum: 101,
+        stName: 'Main St',
+        city: 'Vancouver BC'
+      },
+      size: 70,
+      price: 30000,
+      dateAvailable: '2025-09-01',
+      img: '/assets/home.png'
+    }
+  ];
+
+  constructor(private router: Router) {}
+
+  goToFlatEdit(flat: any) {
+    this.router.navigate(['/flat-view', flat.id])
+  }
+
+  onDelete(flat: any) {
+    //
+  }
 }
