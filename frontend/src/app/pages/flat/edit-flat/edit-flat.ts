39d5afc06500ec3e483479ee42058a5d1317ac12
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-08-23 15:35:06 -0700

    250823: flatview&editflat connect DB

--- frontend/src/app/pages/flat/edit-flat/edit-flat.ts
+++ frontend/src/app/pages/flat/edit-flat/edit-flat.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FlatService, Flat } from '../../../services/flat.service';
+import { UserService, User } from '../../../services/user.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-edit-flat',
@@ -11,79 +15,107 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angula
 
 export class EditFlat {
   editFlatForm = new FormGroup({
-    built: new FormControl<number>(0, [
+    year: new FormControl<number>(2000, [
       Validators.required,
-      Validators.min(0)
+      Validators.min(1800)
     ]),
     city: new FormControl("", [
       Validators.required,
       Validators.minLength(2)
     ]),
-    dateAvailable: new FormControl("", [
+    availDate: new FormControl("", [
       Validators.required
     ]),
-    hasAC: new FormControl("", [
+    hasAC: new FormControl<boolean>(false, [
       Validators.required
     ]),
-    price: new FormControl<number>(0, [
+    price: new FormControl<number>(1000, [
       Validators.required,
-      Validators.min(0)
+      Validators.min(1)
     ]),
-    size: new FormControl<number>(0, [
+    size: new FormControl<number>(50, [
       Validators.required,
-      Validators.min(0)
+      Validators.min(1)
     ]),
     stName: new FormControl("", [
       Validators.required,
       Validators.minLength(2)
     ]),
-    stNum: new FormControl<number>(0, [
+    stNum: new FormControl<number>(1, [
       Validators.required,
-      Validators.min(0)
+      Validators.min(1)
     ])
   })
 
-  get built() { return this.editFlatForm.get('built'); }
+  get year() { return this.editFlatForm.get('year'); }
   get city() { return this.editFlatForm.get('city'); }
-  get dateAvailable() { return this.editFlatForm.get('dateAvailable'); }
+  get availDate() { return this.editFlatForm.get('availDate'); }
   get hasAC() { return this.editFlatForm.get('hasAC'); }
   get price() { return this.editFlatForm.get('price'); }
   get size() { return this.editFlatForm.get('size'); }
   get stNum() { return this.editFlatForm.get('stNum'); }
   get stName() { return this.editFlatForm.get('stName'); }
 
-  flatFromDB = {
-    address: { 
-      stNum: 233, 
-      stName: 'Robson St', 
-      city: 'Vancouver BC' 
-    },
-    price: 950,
-    size: 60,
-    hasAC: 'yes',
-    built: 2012,
-    dateAvailable: '2025-08-26'
-  };
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private flatService: FlatService,
+    private userService: UserService,
+    private authService: AuthService
+  ) { }
+
+  flatId!: string;
 
   ngOnInit() {
-    this.editFlatForm.patchValue({
-      stNum: this.flatFromDB.address.stNum,
-      stName: this.flatFromDB.address.stName,
-      city: this.flatFromDB.address.city,
-      price: this.flatFromDB.price,
-      size: this.flatFromDB.size,
-      hasAC: this.flatFromDB.hasAC,
-      built: this.flatFromDB.built,
-      dateAvailable: this.flatFromDB.dateAvailable
-    });
+    this.flatId = this.route.snapshot.paramMap.get('id')!;
+
+    if (this.flatId) {
+      this.flatService.getFlatById(this.flatId).subscribe(flat => {
+        this.editFlatForm.patchValue({
+          year: flat.year,
+          city: flat.city,
+          availDate: flat.availDate.toString().substring(0, 10),
+          hasAC: flat.hasAC,
+          price: flat.price,
+          size: flat.size,
+          stName: flat.stName,
+          stNum: flat.stNum
+        });
+      });
+    }
   }
 
   onSubmit() {
-    if (this.editFlatForm.valid) {
-      //TODO: update flatData to Database
-      alert('Your flat has been successfully updated!');
+    if (this.editFlatForm.valid && this.flatId) {
+
+      const currentUser = this.authService.getUser();
+      if (!currentUser) return;
+
+      const updatedFlat = {
+        id: this.flatId,
+        city: this.editFlatForm.value.city!,
+        stName: this.editFlatForm.value.stName!,
+        stNum: this.editFlatForm.value.stNum!,
+        size: this.editFlatForm.value.size!,
+        hasAC: this.editFlatForm.value.hasAC!,
+        year: this.editFlatForm.value.year!,
+        price: this.editFlatForm.value.price!,
+        availDate: new Date(this.editFlatForm.value.availDate!),
+        owner: currentUser as any
+      }
+
+      this.flatService.updateFlat(updatedFlat).subscribe({
+        next: () => {
+          alert('Your flat has been successfully updated!');
+          this.router.navigate(['/home']);
+        },
+        error: err => {
+          console.error(err);
+          alert('Failed to update the flat.');
+        }
+      });
     } else {
-      alert('Please fill in all the required information')
+      alert('Please fill in all the required information');
     }
   }
 }
--- frontend/src/app/pages/flat/flat-view/flat-view.ts
+++ frontend/src/app/pages/flat/flat-view/flat-view.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Flat, FlatService } from '../../../services/flat.service';
-import { ActivatedRoute } from '@angular/router';
 import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FlatService, Flat } from '../../../services/flat.service';
+import { UserService, User } from '../../../services/user.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-flat-view',
@@ -10,26 +12,39 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
   styleUrl: './flat-view.css',
 })
 export class FlatView {
-  flat?: Flat;
-
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
-    private flatService: FlatService
-  ) {}
+    private flatService: FlatService,
+    private userService: UserService,
+    private authService: AuthService
+  ) { }
+
+  flat?: Flat;
+  flatId!: string;
+  currentUser?: User| null = null;
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.flatService.getFlatById(id).subscribe({
-        next: (data) => {
-          this.flat = data;
-        },
-        error: (err) => console.error(err),
+    this.flatId = this.route.snapshot.paramMap.get('id')!;
+    this.currentUser = this.authService.getUser(); 
+    if (this.flatId) {
+      this.flatService.getFlatById(this.flatId).subscribe(flat => {
+        this.flat = flat;
       });
     }
   }
 
-  toggleFavorite() {
-    // this.flat.isFavorite = !this.flat.isFavorite;
+  toggleFavorite(user: User, flatId: string) {
+    let updatedFavorites: string[];
+
+    if (user.favorites?.includes(flatId)) {
+      updatedFavorites = user.favorites.filter(id => id !== flatId);
+    } else {
+      updatedFavorites = [...(user.favorites || []), flatId];
+    }
+
+    this.userService.updateFavorites(user._id!, updatedFavorites).subscribe(updatedUser => {
+      this.currentUser = updatedUser;
+    });
   }
 }
